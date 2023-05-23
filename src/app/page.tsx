63dd1ae7c476d77accda72e752675d4ad4108bab
Trentus <trent@iamtrent.us>
2023-05-22 21:41:45 -0400

    trying react sprint parallax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+'use client';
+import styles from './page.module.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import Image from 'next/image';
+import coffeeOne from '../images/coffee-1.jpg';
+import coffeeTwo from '@images/coffee-2.jpg';
+import coffeeThree from '@images/coffee-3.jpg';
+import coffeeFour from '@images/coffee-4.jpg';
+import coffeeFive from '@images/coffee-5.jpg';
+
 export default function Home() {
-    return <div className="">Test</div>;
+    return (
+        <Parallax pages={2}>
+            <ParallaxLayer offset={0} speed={0.5}>
+                <img src={`url${coffeeOne}`} alt="" />
+            </ParallaxLayer>
+            <ParallaxLayer
+                offset={0}
+                speed={1}
+                factor={1.5}
+                style={{
+                    backgroundImage: `url(${coffeeOne})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    width: '100%',
+                }}
+            >
+                <h1 className={`${styles.header} ${styles.latte}`}>THIS IS A LATTE</h1>
+            </ParallaxLayer>
+            <ParallaxLayer offset={1} speed={1}>
+                <h1 className={`${styles.header} ${styles.latte}`}>THIS IS CAPUCCINO</h1>
+            </ParallaxLayer>
+        </Parallax>
+    );
 }
